9549f2c194fae02f77ccb827432b085cea02c1e1
Vlatko Stojkoski <stojvlatko@gmail.com>
2020-11-15 14:26:57 +0100

    Fixed task folder generator README's

--- new-kata.sh
+++ new-kata.sh
@@ -2,4 +2,4 @@ code .
 mkdir "$1"
 cd "$1"
 touch "$1.js" "$1.test.js" "README.md"
-echo "(Kata Link)[https://www.codewars.com/kata/$2]" > README.md
+echo "[Kata Link](https://www.codewars.com/kata/$2)" > README.md
